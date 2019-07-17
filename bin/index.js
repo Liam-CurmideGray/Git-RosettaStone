1cf5a5b8baa399afe0baf0c595ef612085462685
Liam-CurmideGray <liam.curmi@gig.com>
2019-07-17 11:11:45 +0200

    Final Js Starwars

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 
-const axios = require('axios');
-const inquirer = require('inquirer');
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-getAPI();
+getAPI().then(response => InquirerQ(response));
 
 async function getAPI() {
-
   let StarshipUrl = `https://swapi.co/api/starships/`;
   let starShipResponse = [];
 
   while (StarshipUrl != null) {
-
     const starShipsAPI = await axios.get(StarshipUrl);
 
     for (i = 0; i < starShipsAPI.data.results.length; i++) {
@@ -25,59 +23,58 @@ async function getAPI() {
           url: PilotUrls
         }
       });
-
     }
     StarshipUrl = starShipsAPI.data.next;
-
   }
-  InquirerQ(starShipResponse);
+  return starShipResponse;
 }
-
 
 function InquirerQ(starShipResponse) {
   inquirer.prompt(Questions(starShipResponse)).then(answers => {
-    console.info('Ship Selected:', answers.Starships.name);
-    console.info('Pilot Selected:', answers.Pilots.name);
-    console.info('Pilot Details', answers.Pilots);
+    console.info("Ship Selected:", answers.Starships.name);
+    console.info("Pilot Selected:", answers.Pilots.name);
+    console.info("Pilot Details", answers.Pilots);
   });
 }
 
 function Questions(starShipResponse) {
-  return [{
-    type: 'list',
-    name: 'Starships',
-    message: 'Select a Starship',
-    choices: starShipResponse
-  },
-  {
-    type: 'list',
-    name: 'Pilots',
-    message: 'Select Pilot',
-    choices(answers) { return GetPilots(answers.Starships) }
-  }];
-
+  return [
+    {
+      type: "list",
+      name: "Starships",
+      message: "Select a Starship",
+      choices: starShipResponse
+    },
+    {
+      type: "list",
+      name: "Pilots",
+      message: "Select Pilot",
+      choices(answers) {
+        return GetPilots(answers.Starships);
+      }
+    }
+  ];
 }
 
 async function GetPilots(StarshipKey) {
-  StarshipKey = StarshipKey.url;
+  const PilotUrls = StarshipKey.url;
 
   let array = [];
 
-  if (StarshipKey.length === 0) {
+  if (PilotUrls.length === 0) {
     array.push({
       name: "No Pilots",
       value: {
-        name: "No Details found",
-
+        name: "No Details found"
       }
     });
     return array;
   }
 
-  for (i = 0; i < StarshipKey.length; i++) {
-    let pilot = await axios({
-      url: StarshipKey[i],
-      method: 'get'
+  for (i = 0; i < PilotUrls.length; i++) {
+    const pilot = await axios({
+      url: PilotUrls[i],
+      method: "get"
     });
     array.push({
       name: pilot.data.name,
@@ -87,6 +84,5 @@ async function GetPilots(StarshipKey) {
       }
     });
   }
-  return array
-
-};
+  return array;
+}
